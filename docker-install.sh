e1a21f6efd9d1523da16359832335d3e2f9f6da3
kx1t <ramonkolb@gmail.com>
2021-08-10 16:50:24 -0400

    updated verbiage

--- docker-install.sh
+++ docker-install.sh
@@ -28,7 +28,7 @@ echo "If you haven't added your current login to the \"sudoer\" list,"
 echo "you may be asked for your password at various times during the installation."
 echo
 read -p "Press ENTER to start, or CTRL-C to abort"
-echo -n "Checking for Docker installation... "
+echo -n "Checking for an existing Docker installation... "
 if which docker >/dev/null 2>1
 then
     echo "found! No need to install..."
