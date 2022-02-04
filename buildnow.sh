6a54b1a8905ebcf5563738e822c7f492e1bac36d
kx1t <15090643+kx1t@users.noreply.github.com>
2022-02-04 17:31:28 -0500

    Dev (#26)
    * changed base image to ghcr.io/sdr-enthusiasts/docker-baseimage:python; moved arch check to container runtime
    
    * remove docker-install from repo
    
    * build script updates
    
    * build script updates

--- buildnow.sh
+++ buildnow.sh
@@ -6,21 +6,12 @@ set -x
 [[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
 [[ "$ARCHS" == "" ]] && ARCHS="linux/armhf,linux/arm64,linux/amd64"
 
+IMAGE=kx1t/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG
+echo "press enter to start building $IMAGE from $BRANCH"
+read
+
 # rebuild the container
-pushd ~/git/docker-radarvirtuel
 git checkout $BRANCH || exit 2
-
-# make the build certs root_certs folder:
-# Note that this is normally done as part of the github actions - we don't have those here, so we need to do it ourselves before building:
-#ls -la /etc/ssl/certs/
-#mkdir -p ./root_certs/etc/ssl/certs
-#mkdir -p ./root_certs/usr/share/ca-certificates/mozilla
-
-#cp -P /etc/ssl/certs/*.crt ./root_certs/etc/ssl/certs
-#cp -P /etc/ssl/certs/*.pem ./root_certs/etc/ssl/certs
-#cp -P /usr/share/ca-certificates/mozilla/*.crt ./root_certs/usr/share/ca-certificates/mozilla
-
 git pull -a
-docker buildx build --progress=plain --compress --push $2 --platform $ARCHS --tag kx1t/radarvirtuel:$TAG .
-#rm -rf ./root_certs
-popd
+
+docker buildx build --compress --push $2 --platform $ARCHS --tag $IMAGE .
