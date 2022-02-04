efb3b394ae36d2c51613af892aebd4525b5b7b11
kx1t <ramonkolb@gmail.com>
2022-02-04 17:28:49 -0500

    build script updates

--- buildnow.sh
+++ buildnow.sh
@@ -6,8 +6,11 @@ set -x
 [[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
 [[ "$ARCHS" == "" ]] && ARCHS="linux/armhf,linux/arm64,linux/amd64"
 
+IMAGE=kx1t/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG
+echo "press enter to start building $IMAGE from $BRANCH"
+read
+
 # rebuild the container
-pushd ~/git/docker-radarvirtuel
 git checkout $BRANCH || exit 2
 
 # make the build certs root_certs folder:
@@ -21,6 +24,5 @@ git checkout $BRANCH || exit 2
 #cp -P /usr/share/ca-certificates/mozilla/*.crt ./root_certs/usr/share/ca-certificates/mozilla
 
 git pull -a
-docker buildx build --progress=plain --compress --push $2 --platform $ARCHS --tag kx1t/radarvirtuel:$TAG .
+docker buildx build --compress --push $2 --platform $ARCHS --tag $IMAGE .
 #rm -rf ./root_certs
-popd
