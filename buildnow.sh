b01647f4fa5fee39891d17e366bfaa64b3a646f6
kx1t <kx1t@amsat.org>
2023-01-23 17:20:23 -0500

    update build script

--- buildnow.sh
+++ buildnow.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#shellcheck shell=bash disable=SC2086,SC2015
 #
 set -x
 
@@ -9,14 +10,16 @@ set -x
 BASETARGET1=ghcr.io/sdr-enthusiasts
 BASETARGET2=kx1t
 
-IMAGE1="$BASETARGET1/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
-IMAGE2="$BASETARGET2/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
+# IMAGE1="$BASETARGET1/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
+# IMAGE2="$BASETARGET2/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
+IMAGE1="$BASETARGET1/$(pwd):$TAG"
+IMAGE2="$BASETARGET2/$(pwd):$TAG"
 
 [[ "$IMAGE1" == "$BASETARGET1/reversewebproxy:$TAG" ]] && IMAGE1="$BASETARGET1/webproxy:$TAG" || true
 [[ "$IMAGE2" == "$BASETARGET2/reversewebproxy:$TAG" ]] && IMAGE2="$BASETARGET2/webproxy:$TAG" || true
 
 echo "press enter to start building $IMAGE1 and $IMAGE2 from $BRANCH"
-read
+read -r
 
 starttime="$(date +%s)"
 # rebuild the container
